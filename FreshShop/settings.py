9f42678c3fb18097a7275b8002b45f9835d46ecd
devika <devikaunnikrishnan12@gmail.com>
2026-01-15 09:50:47 +0530

    Fix 404 error by adding login/logout redirect URLs

--- FreshShop/settings.py
+++ FreshShop/settings.py
@@ -128,3 +128,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Authentication redirects
+LOGIN_REDIRECT_URL = 'product_list'
+LOGOUT_REDIRECT_URL = 'product_list'
